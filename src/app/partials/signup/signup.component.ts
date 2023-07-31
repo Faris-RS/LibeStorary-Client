3c53e229bb074a72c5e2bcd799fcb365ff0b8891
Mohamed Faris <brotofarisrs@gmail.com>
2023-07-31 08:48:03 +0530

    ui cleanup

--- src/app/partials/signup/signup.component.ts
+++ src/app/partials/signup/signup.component.ts
@@ -18,6 +18,9 @@ export class SignupComponent {
     console.log('Form submitted!');
     console.log('Email:', this.email);
     console.log('Password:', this.password);
+    console.log('Confirm Passowrd:', this.confirmPassword);
+    console.log('First Name:', this.firstName);
+    console.log('Last Name:', this.lastName);
     if (this.password === this.confirmPassword) {
       this.ContinueToOTP();
     }
